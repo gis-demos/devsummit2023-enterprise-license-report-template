6b914af0c7eb909e85eb995b7407e4df63392b0a
Jay Roebuck <jroebuck@esri.com>
2023-02-21 12:47:27 -0500

    send email report

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
 import sys
 import arcgis
 from arcgis.gis import GIS
 from pprint import pprint
 import datetime as dt
+import smtplib
 
 # Create the datetime representing current time
 now = dt.datetime.now()
@@ -27,20 +29,74 @@ def datetime_of_timestamp(timestamp_value) -> dt.datetime:
     return dt_value
 
 def get_server_licenses(gis):
-    hosting_server = gis.admin.servers.get(role="HOSTING_SERVER")
-    if not hosting_server:
+    hosting_servers = gis.admin.servers.get(role="HOSTING_SERVER")
+    if not hosting_servers:
         # no hosting server configured for gis instance
         return {}
-    return hosting_server.system.licenses
+    return hosting_servers[0].system.licenses
 
 def get_license_status(gis):
-    days_30 = dt.timedelta(days=30) # timedelta object representing a 30 day time period
+    """
+    Returns a dict of feature name keys
+    with values of True if license is expiring/expired,
+    or False if the license is valid through the threshold
+    """
     server_license_status = {}
     for feature_license in get_server_licenses(gis).get('features', []):
         date_of_expiration = datetime_of_timestamp(feature_license['expiration'])
-        if date_of_expiration - now <= days_30:
-            server_license_status[feature_license['displayName']] = expires_in_30_days_or_expired(date_of_expiration)
+        is_expiring_concern = expires_in_30_days_or_expired(date_of_expiration)
+        server_license_status[feature_license['displayName']] = is_expiring_concern
     return server_license_status
+
+def send_report_email(license_status):
+    license_summary = "\r\n".join([f"{i[0]}: {'Requires Attention' if i[1] else 'Valid'}" for i in license_status.items()])
+    message = f"""
+ArcGIS Licenses Report:
+
+{license_summary}
+    """
+    send_smtp(["jroebuck@esri.com", "ssong@esri.com"], message)
+
+def send_smtp(recipients, message,
+                    subject="ArcGIS Licensing Report"):
+    """Sends the `message` string to all of the emails in the 
+    `recipients` using the configured SMTP email server."""
+    smtp_config = get_smtp_config()
+    smtp_server_url = smtp_config.get('server')
+    sender= smtp_config.get('username')
+    username = smtp_config.get('username')
+    password = smtp_config.get('password')
+
+    # Instantiate our server, configure the necessary security
+    server = smtplib.SMTP(smtp_server_url, 587)
+    server.ehlo()
+    server.starttls() # Needed if TLS is required for the SMTP server
+    server.login(username, password)
+
+    # For each recipient, construct the message and attempt to send
+    did_succeed = True
+    for recipient in recipients:
+        try:
+            message_body = '\r\n'.join(['To: {}'.format(recipient),
+                                        'From: {}'.format(sender),
+                                        'Subject: {}'.format(subject),
+                                        "",
+                                        '{}'.format(message)])
+            server.sendmail(sender, [recipient], message_body)
+            print(f"Report sent to {recipient}")
+        except:
+            did_succeed = False
+    
+    # Cleanup and return
+    server.quit()
+    return did_succeed
+
+def get_smtp_config():
+    return {
+        "server": os.environ.get("SMTP_SERVER"),
+        "username": os.environ.get("SMTP_USERNAME"),
+        "password": os.environ.get("SMTP_PASSWORD"),
+    }
 
 def get_main_args():
     args = sys.argv[-3:] # last 3 args
@@ -58,6 +114,8 @@ def main():
     url, username, password = get_main_args()
     gis:GIS = GIS(url=url, username=username, password=password)
     license_status = get_license_status(gis)
+    send_report_email(license_status)
+    
     expiring_features = [feature for feature in license_status if license_status[feature] == True]
     if len(expiring_features) > 0:
         # print features expiring
